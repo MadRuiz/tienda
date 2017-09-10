6dacbf35da423ea4d9817b804728178defe22118
MadRuiz <madelynruiz06@gmail.com>
2017-09-10 14:20:59 -0600

    conexion 2.0

--- conexion.php
+++ conexion.php
@@ -1,3 +1,3 @@
 <?php
-$conexion = mysqli_connect("130211.210.230:3306", "acmon","Kurosaki","tiendaonline");
+$conexion = mysqli_connect("130.211.210.230:3306", "acmon","Kurosaki","tiendaonline");
 mysqli_set_charset($conexion, "UTF-8");
