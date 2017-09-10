62cd04a55f19c5d80109a644f2a8f78a746cc4b3
MadRuiz <madelynruiz06@gmail.com>
2017-09-10 14:15:41 -0600

    conexion

--- conexion.php
+++ conexion.php
@@ -1,3 +1,3 @@
 <?php
-$conexion = mysqli_connect("localhost", "madruiz","madruiz","tiendaonline");
+$conexion = mysqli_connect("130211.210.230:3306", "acmon","Kurosaki","tiendaonline");
 mysqli_set_charset($conexion, "UTF-8");
